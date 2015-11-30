815a7ac210ce9bd530beec39740f51f8b36ab057
liqingshan <liqingshan@rtmap.com>
2015-11-30 12:16:10 +0800

    新增配置文件

---
+++ src/main/java/com/rtmap/traffic/bcm/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.rtmap.traffic.bcm.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	Logger logger = LoggerFactory.getLogger("com.rtmap.traffic.bcm.controller.MainController");
+    
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String login(HttpServletRequest request, Model model){
+	    logger.debug("login");
+	    
+		model.addAttribute("welcome", "hello world");
+
+		return "login";
+	}
+}
