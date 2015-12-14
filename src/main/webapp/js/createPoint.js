1c983ad7829c6b82a38f40e80f640290767e06d9
liqingshan <liqingshan@rtmap.com>
2015-12-14 19:04:37 +0800

    graph dev

--- src/main/webapp/js/createPoint.js
+++ src/main/webapp/js/createPoint.js
@@ -1,18 +1,27 @@
-function createPoint(arr)
-{
+function createPoint(arr) {
 	$('#svg-map').html('');
-	
-	setTimeout(function(){
-		for(var i=0; i<arr.length; i++)
-		{
-			var oC=document.createElementNS('http://www.w3.org/2000/svg','circle');
-			$(oC).attr({
-				'cx':Math.floor((arr[i][0]+14492)/1211*$('#map').width()/945),
-				'cy':Math.floor((Math.abs(arr[i][1])-74620)/1220*$('#map').height()/630),
-				'r':5,
-				'fill':'red'
-			});
+
+	setTimeout(function() {
+		for (var i = 0; i < arr.length; i++) {
+			var oC = document.createElementNS('http://www.w3.org/2000/svg',
+					'circle');
+			var color;
+			if (i == 0) {
+				color = 'green';
+			} else {
+				color = 'red';
+			}
+
+			$(oC).attr(
+					{
+						'cx' : Math.floor((arr[i][0] + 14492) / 1211
+								* $('#map').width() / 945),
+						'cy' : Math.floor((Math.abs(arr[i][1]) - 74620) / 1220
+								* $('#map').height() / 630),
+						'r' : 5,
+						'fill' : color
+					});
 			$(oC).appendTo($('#svg-map'));
 		}
-	},200)
+	}, 200)
 }
